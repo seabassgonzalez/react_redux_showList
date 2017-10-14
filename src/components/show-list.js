4995f0e2b26490ba5bf44b43c94d1701a9f377f5
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-10-14 13:49:32 -0700

    Flesh out ShowList component

--- src/components/show-list.js
+++ src/components/show-list.js
@@ -7,3 +7,13 @@
 
 import React, { Component } from 'react';
 
+export default class ShowList extends Component {
+	render(){
+		return (
+			<ul className-"list-group col-sm-4">
+
+			</ul>
+		)
+	}	
+}
+
