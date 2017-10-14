4b6b7735f7cb8cb5776ea145421bd9e28dab6e65
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-10-14 13:34:18 -0700

    Initial actions components and reducers directories, reducer_shows that exports an array of media objects

---
+++ src/reducers/reducer_shows.js
@@ -0,0 +1,11 @@
+// simple function that 
+	// returns an array of books
+
+function(){
+	return [
+		{ title: 'Seinfeld' },
+		{ title: 'Rick and Morty' },
+		{ title: 'Curb Your Enthusiasm' },
+		{ title: 'The Orville' }
+	]
+}
