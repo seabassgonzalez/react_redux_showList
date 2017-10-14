21ce2c0782d29e816b57c254994efdf97620a1ec
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-10-14 16:49:33 -0700

    Basic component App

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+// import react and component
+
+// export App class extending Component
+	// render
+		// return
+			// div		
+
+import React, { Component } from 'react';
+
+export default class App extends Component{
+	render(){
+		return(
+			<div>Media Library</div>
+		);
+	}
+} 
