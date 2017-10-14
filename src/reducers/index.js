5e133146a97ae9ee131678e1be5b30219d0f492a
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-10-14 13:43:34 -0700

    Import shows reducer and set to shows in state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,15 @@
 // import combineReducers from redux
-
+// import ShowsReducer
+// 
 // constant rootReducer to use combineReducers
-
+	// map state -- shows is showsReducer
 // export rootReducer
 
 import { combine Reducers } from 'redux';
+import ShowsReducer from './reducer_shows';
 
 const rootReducer = combineReducers({
-
+	shows: ShowsReducer
 });
 
 export default rootReducer;
