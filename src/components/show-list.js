7edff4fe6ef53bcb79fd692bad0f60d880ac5f67
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-10-14 13:47:45 -0700

    Initial show list component

---
+++ src/components/show-list.js
@@ -0,0 +1,9 @@
+// import React, Component
+
+// export Booklist class extending Component
+	// renders
+		// return
+			// ul classname list-group col-sm-4
+
+import React, { Component } from 'react';
+
