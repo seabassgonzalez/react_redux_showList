73674680519e4950034200acb5705155357b0585
seabassgonzalez <seabassgonzalez@Sebastians-MacBook-Pro.local>
2017-10-14 13:38:48 -0700

    Initial rootReducer index.js

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+// import combineReducers from redux
+
+// constant rootReducer to use combineReducers
+
+// export rootReducer
+
+import { combine Reducers } from 'redux';
+
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;
