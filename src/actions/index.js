54a86e05a6db89ea115eb778c263472cacf66c7b
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-05 21:13:04 -0800

    Console log statement to test user click functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@
 
 export function selectShow(show){
 	// selectShow is an action creator, needs to return an action- object with type property
+	console.log('A show has been selected:', show.title);
 	return{
 		type: "SHOW_SELECTED",
 		payload: show
