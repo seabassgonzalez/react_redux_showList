d759dfdac5b779186b53a5c5b007b7c407459351
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-05 21:09:42 -0800

    Initial implementation of library and component imports

--- src/index.js
+++ src/index.js
@@ -11,3 +11,10 @@
 // ReactDOM.render
 	// Provider store = {createStoreWithMiddleware(reducers)}
 	// document.querySelector(.container)
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import App from './components/app';
+import reducers from './reducers';
