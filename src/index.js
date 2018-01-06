4f53751ba90dbbffb7c3b00543ff780437d7548d
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-05 21:07:29 -0800

    Initial pseudocode for root index.js importing React, ReactDOM, Provider from react-redux, createStore and applyMiddleware from redux, import App and reducers, then render all to dom at .container

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-// export default function, all reducers get two arguments -- state and action
-// state argument is not application state, just what this reducer is responsible for
-// use es6 syntax to default state to null in case it comes in undefined
-  // switch case on action.type
-    // if type is SHOW_SELECTED
-      // return action payload
-  // all else return state
+// import React from react
+// import ReactDOM from react-dom
+// import Provider from react-redux
+// import createStore and applyMiddleware from redux
 
-  // testing git config
-  
-export default function(state = null, action){
-  switch(action.type){
-    case'SHOW_SELECTED':
-      return action.payload;
-  }
-  return state;
-}
+// import App
+// import reducers
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+// ReactDOM.render
+	// Provider store = {createStoreWithMiddleware(reducers)}
+	// document.querySelector(.container)
